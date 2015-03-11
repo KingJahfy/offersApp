8179c03240f8218d75c90cbbdb55b9fd5b007a50
Tinatua Kolleh <tinatuh@gmail.com>
2015-03-10 22:48:47 -0400

    using local storage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 * Author: Tinatuh Kolleh
 * Display offers to facebook users
 */
+$(function (){
 var Offer = Backbone.Model.extend({
 	defaults: function (){
 		return{	
@@ -28,7 +29,7 @@ var OfferList = Backbone.Collection.extend({
 	}
 });
 
-var Offers = new OfferList;
+var offers = new OfferList;
 
 var OfferView = Backbone.View.extend({
 	tagName: 'li',
@@ -41,7 +42,7 @@ var OfferView = Backbone.View.extend({
 	},
 	render: function (){
 		var source = $('#offerTemplate').html();
-		var templte = Handlebars.compile(source);
+		var template = Handlebars.compile(source);
 		var html = template(this.model.toJSON());
 
 		this.$el.html(html);
@@ -58,10 +59,10 @@ var AppView = Backbone.View.extend({
 		"click #selectedOffers": "toggleAllSelected"
 	},
 	initialize: function (){
-		this.listenTo(Offers, 'add', this.addOne);
-		this.listenTo(Offers, 'reset', this.addAll);
-		this.listenTo(Offers, 'all', this.render);
-		Offers.fetch();
+		this.listenTo(offers, 'add', this.addOne);
+		this.listenTo(offers, 'reset', this.addAll);
+		this.listenTo(offers, 'all', this.render);
+		offers.fetch();
 	},
 	addOne: function (offer){
 		var view = new OfferView({
@@ -70,9 +71,26 @@ var AppView = Backbone.View.extend({
 		this.$('#offer-list').append(view.render().el);
 	},
 	addAll: function (){
-		Offers.each(this.addOne, this);
+		offers.each(this.addOne, this);
 	},
 	render: function (){
 	}
 });
 
+var App = new AppView;
+var offerA = new Offer;
+	offer.set({	
+		title: "Free Two Liter Bottle",
+		provider: "Coca Cola",
+		selected: false,
+		available: true
+	});
+var offerB = new Offer;
+	offer.set({	
+		title: "50% Off Air Jordan",
+		provider: "Nike",
+		selected: false,
+		available: true
+	});
+offers.push('')
+});
